db00acadf0c26267d3973d682d0aaa79b9916cda
Darya <dusenka.tikhonova@gmail.com>
2019-04-26 18:38:08 +0300

    hw5

--- src/app/components/dz__5/dz.js
+++ src/app/components/dz__5/dz.js
@@ -15,17 +15,26 @@ var arrayOfItems = [
 
     {img: 'src/static/img/team/2.jpg', year: 2017, model: 'Samsung'},
 
-    {img: 'src/static/img/team/1.jpg',year: 2015, model: 'Xiaomi'}
+    {img: 'src/static/img/team/1.jpg', year: 2015, model: 'Xiaomi'}
 ];
-$(document).ready(function(){
-    var filterItems = $('.filter__items')
-    arrayOfItems.forEach(function(item, index) {
-        filterItems.append(`
-        <div class = "f__item"> 
-        <p>+ item.year +</p>
-        <p>+ item.model +</p>
-        </div>
-    `)
-  })
+$(document).ready(function() {
+var filterHolder = $('.filter__items');
+arrayOfItems.forEach(function(item, index){
+  filterHolder.append('<div class="f__item"> <img src='+item.img+'> <p>'+item.year+'</p><p>'+item.model+'</p></div>')
+})
 });
 
+  $('#year').change(
+    function(e){
+                if(e.target.options[e.target.selectedIndex].text) {
+                    var filteredArray = arrayOfItems.filter( function(item) {
+                    return this.year <2016 && this.year.remove> 2016;
+                })
+            }
+    
+                var filterHolder = $('.filterHolder'); 
+      filterHolder.html('');
+         filteredArray.forEach(function(item, index) {
+            filterHolder.append('<div class= "personIt"><img src='+item.image+'><p>'+item.model+'</p> <p>'+item.year+'</p></div>')
+         }) 
+        });
