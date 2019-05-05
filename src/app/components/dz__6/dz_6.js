a6c01a36b57ac35a4278d93a6bf4972266fdadde
Darya <dusenka.tikhonova@gmail.com>
2019-05-05 21:04:35 +0300

    part without pagination and filter

--- src/app/components/dz__6/dz_6.js
+++ src/app/components/dz__6/dz_6.js
@@ -1,14 +1,17 @@
 import $ from 'jquery'
+import 'bootstrap' 
 
-var news = $('.news'); 
-          
-$(function () {
-    $.getJSON('https://jsonplaceholder.typicode.com/posts', function (data) {
-        var news = $('.news');
-        var newList = [];
-        $.each(data, function (key, val) {
-            newList.append('<tr><td>' + data.title + '<tr><td>' + data.body + '</td><td>' + data.id +
-                '</td><td>');
-        })
-    })
+$(document).ready(function(){
+    $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data) {
+        var news_data = '';
+            $.each(data, function(key, value){
+                news_data += '<tr>';
+                news_data += '<td>' +value.title+ '</td>';
+                news_data += '<td>' +value.body+ '</td>';
+                news_data += '<td>' +value.userId+ '</td>';
+                news_data += '</tr>';
+            });
+        $('#news').append(news_data);
+    });
 });
+
