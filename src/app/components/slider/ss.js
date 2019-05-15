0eaf7ec7d3f208866b11028f07a2116fc433c52b
Darya <dusenka.tikhonova@gmail.com>
2019-05-15 13:40:23 +0300

    slider inrasructure added

--- src/app/components/slider/ss.js
+++ src/app/components/slider/ss.js
@@ -1,1 +1,22 @@
-      
+import $ from 'jquery';
+import 'slick-carousel';
+
+$(document).ready(function(){
+    $('.gallery-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.gallery-nav'
+      });
+      $('.gallery-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.gallery-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        arrows: false
+     
+      });
+    });
